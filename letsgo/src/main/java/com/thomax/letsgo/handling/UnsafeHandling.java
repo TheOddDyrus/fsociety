3e012853777b9c9948c91cc7a5b29b71846ab935
_thomax <372543590@qq.com>
2019-01-30 17:27:45 +0800

    add Unsafe

---
+++ letsgo/src/main/java/com/thomax/letsgo/handling/UnsafeHandling.java
@@ -0,0 +1,32 @@
+package com.thomax.letsgo.handling;
+
+import sun.misc.Unsafe;
+
+import java.lang.reflect.Field;
+
+/**
+ * Unsafe类是在sun.misc包下，不属于Java标准。但是很多Java的基础类库，包括一些被广泛使用的高性能开发库都是基于Unsafe类开发的，
+ * 比如Netty、Cassandra、Hadoop、Kafka等。Unsafe类在提升Java运行效率，增强Java语言底层操作能力方面起了很大的作用
+ * 1.内存管理
+ * 2.非常规的对象实例化
+ * 3.操作类、对象、变量
+ * 4.数组操作
+ * 5.多线程同步
+ * 6.挂起与恢复
+ * 7.内存屏障
+ */
+public class UnsafeHandling {
+
+    private Unsafe unsafe;
+
+    public UnsafeHandling() throws IllegalAccessException, NoSuchFieldException {
+        Field f = Unsafe.class.getDeclaredField("theUnsafe");
+        f.setAccessible(true);
+        unsafe = (Unsafe) f.get(null);
+    }
+
+    public Unsafe getUnsafe() {
+        return unsafe;
+    }
+
+}
