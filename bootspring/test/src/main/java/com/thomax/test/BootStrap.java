f04cab4d6d501a03bf4ff36e2cdfe3818cac8271
_thomax <372543590@qq.com>
2019-02-23 15:56:38 +0800

    add controller to start

--- bootspring/test/src/main/java/com/thomax/test/BootStrap.java
+++ bootspring/test/src/main/java/com/thomax/test/BootStrap.java
@@ -2,12 +2,20 @@ package com.thomax.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class BootStrap {
 
     public static void main(String[] args) {
         SpringApplication.run(BootStrap.class, args);
+    }
+
+    @RequestMapping("/test")
+    public Object test() {
+        return 123;
     }
 
 }
