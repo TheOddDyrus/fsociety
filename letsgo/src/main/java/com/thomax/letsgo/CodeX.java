6c519e3364e5f3d4d87f4cbe56edeb6872414b28
_thomax <372543590@qq.com>
2019-03-01 09:09:54 +0800

    optimize main

--- letsgo/src/main/java/com/thomax/letsgo/CodeX.java
+++ letsgo/src/main/java/com/thomax/letsgo/CodeX.java
@@ -7,29 +7,8 @@ package com.thomax.letsgo;
 public class CodeX {
 
 	public static void main(String[] args) {
-		new T3().function3().function2().function1();
-		System.out.println();
+
 	}
 
 }
 
-class T1 {
-	public void function1() {
-		System.out.println("1");
-
-	}
-}
-
-class T2 {
-	public T1 function2() {
-		System.out.println("2");
-		return new T1();
-	}
-}
-
-class T3 {
-	public T2 function3() {
-		System.out.println("3");
-		return new T2();
-	}
-}
