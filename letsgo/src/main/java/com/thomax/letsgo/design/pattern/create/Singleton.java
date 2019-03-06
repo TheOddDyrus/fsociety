cdc08344b517b740a5e0821554f552d9fcfc7d7a
_thomax <372543590@qq.com>
2019-03-06 11:08:01 +0800

    optimize design pattern - singleton

--- letsgo/src/main/java/com/thomax/letsgo/design/pattern/create/Singleton.java
+++ letsgo/src/main/java/com/thomax/letsgo/design/pattern/create/Singleton.java
@@ -8,32 +8,26 @@ package com.thomax.letsgo.design.pattern.create;
 public class Singleton {
 	public static void main(String[] args) {
 		//单例模式的使用
-		OddOne.getInstance().sayHello();
+		OddOne.getInstance();
 	}
 }
 
 //懒汉模式
 class OddOne {
 	private static OddOne oo = null; //此处赋值为null，目的是实现延迟加载
+
 	private OddOne() {}
-	
-	/*为单例模式设置线程同步
-	   private static synchronized void syncInit() {  
-        if (oo == null) {  
-        	oo = new OddOne();  
-        }  
-       }  
-     */
 	
 	public static OddOne getInstance() {
 		if (oo == null) {
-			oo = new OddOne();  //当需要线程同步时此行代码替换成syncInit();
+			synchronized (OddOne.class) {
+				if (oo == null) {
+					oo = new OddOne();
+				}
+			}
 		}
+
 		return oo;
-	}
-	
-	public void sayHello() {
-		System.out.println("Hello!");
 	}
 }
 
