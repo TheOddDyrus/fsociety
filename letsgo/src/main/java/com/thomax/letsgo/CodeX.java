f8c155220f57c7dc60faa2e1df29bf854187cd1f
_thomax <372543590@qq.com>
2019-03-11 15:02:00 +0800

    update thomax

--- letsgo/src/main/java/com/thomax/letsgo/CodeX.java
+++ letsgo/src/main/java/com/thomax/letsgo/CodeX.java
@@ -1,5 +1,8 @@
 package com.thomax.letsgo;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * 请在此处对LetsGo进行测试！
  * @author _thomas
@@ -7,6 +10,45 @@ package com.thomax.letsgo;
 public class CodeX {
 
 	public static void main(String[] args) {
+		ExecutorService threadPool = Executors.newFixedThreadPool(100);
+
+		for (int i = 0; i < 200; i++) {
+			Runnable runnable1 = () -> {
+				Test.test();
+			};
+			Runnable runnable2 = () -> {
+				Test.test2();
+			};
+
+			threadPool.submit(runnable1);
+			threadPool.submit(runnable2);
+		}
+
+	}
+
+}
+
+class Test {
+
+	public static void test() {
+		System.out.println("A");
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("B");
+	}
+
+	public static void test2() {
+
+			System.out.println("A2");
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println("B2");
 
 	}
 
