ce68f0b80e1043d533e28dcf5b4c32297f873297
_thomax <372543590@qq.com>
2019-03-11 18:39:57 +0800

    optimize conflicts

--- letsgo/src/main/java/com/thomax/letsgo/CodeX.java
+++ letsgo/src/main/java/com/thomax/letsgo/CodeX.java
@@ -10,47 +10,9 @@ import java.util.concurrent.Executors;
 public class CodeX {
 
 	public static void main(String[] args) {
-		ExecutorService threadPool = Executors.newFixedThreadPool(100);
-
-		for (int i = 0; i < 200; i++) {
-			Runnable runnable1 = () -> {
-				Test.test();
-			};
-			Runnable runnable2 = () -> {
-				Test.test2();
-			};
-
-			threadPool.submit(runnable1);
-			threadPool.submit(runnable2);
-		}
 
 	}
 
 }
 
-class Test {
-
-	public static void test() {
-		System.out.println("A");
-		try {
-			Thread.sleep(500);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		System.out.println("B");
-	}
-
-	public static void test2() {
-
-			System.out.println("A2");
-			try {
-				Thread.sleep(500);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			System.out.println("B2");
-
-	}
-
-}
 
