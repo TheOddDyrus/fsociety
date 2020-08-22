e48d2d03c57e417c78fb3da0fe0965f2f48475ed
TheOddDyrus <Forever014>
2020-08-22 21:58:50 +0800

    update

--- dance/shadow-refresh/src/main/java/com/thomax/shadow/refresh/Refresh.java
+++ dance/shadow-refresh/src/main/java/com/thomax/shadow/refresh/Refresh.java
@@ -20,7 +20,6 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
