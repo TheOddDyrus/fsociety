86a925e2740927f1b2d42a97428aa4fb0cd66c0e
TheOddDyrus <372543590@qq.com>
2021-03-04 13:32:26 +0800

    optimize memory size test

--- letsgo/src/main/java/com/thomax/letsgo/advanced/jvm/MemorySizeTest.java
+++ letsgo/src/main/java/com/thomax/letsgo/advanced/jvm/MemorySizeTest.java
@@ -11,30 +11,49 @@ import java.util.concurrent.atomic.AtomicLong;
 /**
  * 对象占用内存测试（单位：byte）
  *
- * Object object = new Object(): object占八个字节，object的引用占四个字节 ==> 一个空对象占用：8byte + 4byte = 12byte
+ * Object object = new Object(): object占八个字节，object的引用占四个字节 ==> 一个空对象占用：8byte + 4byte = 12byte （最后java会补全slot，一个Object=12+4=16个字节）
  */
 public class MemorySizeTest {
 
     public static void main(String[] args) throws ExecutionException {
-        LoadingCache<String, AtomicLong> cache = CacheBuilder.newBuilder()
-                .maximumSize(50000)
-                .build(new CacheLoader<String, AtomicLong>() {
-                    @Override
-                    public AtomicLong load(String key) {
-                        return new AtomicLong(System.currentTimeMillis());
-                    }
-                });
+        Object o = new Object();
+        System.out.println("all byte:" + RamUsageEstimator.sizeOf(o)); //计算指定对象及其引用树上的所有对象的综合大小，单位字节
+        System.out.println("all memory:" + RamUsageEstimator.humanSizeOf(o)); //计算指定对象及其引用树上的所有对象的综合大小，并返回可读的结果
+        System.out.println("root byte:" + RamUsageEstimator.shallowSizeOf(o)); //计算指定对象本身在堆空间的大小，单位字节
+        System.out.println("--------------------");
 
-        for (int i = 0; i < 50400; i++) {
-            String x = "ABCDEFGHIJK" + i;
-            x = x.substring(x.length() - 12);
-            cache.get(x);
+        AA aa = new AA();
+        System.out.println("all byte:" + RamUsageEstimator.sizeOf(aa));
+        System.out.println("all memory:" + RamUsageEstimator.humanSizeOf(aa));
+        System.out.println("root byte:" + RamUsageEstimator.shallowSizeOf(aa));
+        System.out.println("--------------------");
+
+        BB bb = new BB();
+        System.out.println("all byte:" + RamUsageEstimator.sizeOf(bb));
+        System.out.println("all memory:" + RamUsageEstimator.humanSizeOf(bb));
+        System.out.println("root byte:" + RamUsageEstimator.shallowSizeOf(bb));
+    }
+
+    private static class AA {
+        private int num = 123456;
+        public int getNum() {
+            return num;
         }
 
-        System.out.println("size:" + cache.size());
-        System.out.println("all byte:" + RamUsageEstimator.sizeOf(cache)); //计算指定对象及其引用树上的所有对象的综合大小，单位字节
-        System.out.println("all memory:" + RamUsageEstimator.humanSizeOf(cache)); //计算指定对象及其引用树上的所有对象的综合大小，并返回可读的结果
-        System.out.println("root byte:" + RamUsageEstimator.shallowSizeOf(cache)); //计算指定对象本身在堆空间的大小，单位字节
+        public void setNum(int num) {
+            this.num = num;
+        }
+    }
+
+    private static class BB {
+        private long num = 123456L;
+        public long getNum() {
+            return num;
+        }
+
+        public void setNum(long num) {
+            this.num = num;
+        }
     }
 
 }
