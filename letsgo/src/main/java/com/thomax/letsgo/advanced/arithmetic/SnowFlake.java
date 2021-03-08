14934aee908c2eb2cbd9abb4667552411c9df9df
TheOddDyrus <372543590@qq.com>
2021-03-08 16:19:11 +0800

    update snow flake

--- letsgo/src/main/java/com/thomax/letsgo/advanced/arithmetic/SnowFlake.java
+++ letsgo/src/main/java/com/thomax/letsgo/advanced/arithmetic/SnowFlake.java
@@ -8,36 +8,38 @@ package com.thomax.letsgo.advanced.arithmetic;
  */
 public class SnowFlake {
 
-    /**
-     * 起始的时间戳
-     */
+    //起始的时间戳
     private final static long twepoch = 1557825652094L;
 
-    /**
-     * 每一部分占用的位数
-     */
+    //每一部分占用的位数
     private final static long workerIdBits = 5L;
     private final static long datacenterIdBits = 5L;
     private final static long sequenceBits = 12L;
 
-    /**
-     * 每一部分的最大值
-     */
-    private final static long maxWorkerId = -1L ^ (-1L << workerIdBits);
-    private final static long maxDatacenterId = -1L ^ (-1L << datacenterIdBits);
-    private final static long maxSequence = -1L ^ (-1L << sequenceBits);
+    //每一部分的最大值
+    private final static long maxWorkerId = ~(-1L << workerIdBits); //-1L ^ (-1L << workerIdBits)
+    private final static long maxDatacenterId = ~(-1L << datacenterIdBits);
+    private final static long maxSequence = ~(-1L << sequenceBits);
 
-    /**
-     * 每一部分向左的位移
-     */
+    //每一部分向左的位移
     private final static long workerIdShift = sequenceBits;
     private final static long datacenterIdShift = sequenceBits + workerIdBits;
     private final static long timestampShift = sequenceBits + workerIdBits + datacenterIdBits;
 
-    private long datacenterId; // 数据中心ID
-    private long workerId; // 机器ID
+    private final long datacenterId; // 数据中心ID
+    private final long workerId; // 机器ID
     private long sequence = 0L; // 序列号
     private long lastTimestamp = -1L; // 上一次时间戳
+
+    public static void main(String[] args) {
+        for (long i = 0; i < 3; i++) {
+            long finalI = i;
+            new Thread(() -> {
+                SnowFlake snowFlake = new SnowFlake(finalI, 10);
+                System.out.println(snowFlake.nextId());
+            }).start();
+        }
+    }
 
     public SnowFlake(long workerId, long datacenterId) {
         if (workerId > maxWorkerId || workerId < 0) {
