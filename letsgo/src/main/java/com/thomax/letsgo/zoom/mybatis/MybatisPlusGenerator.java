00b6e99e0252b2b921cac33ccb5ee78b2775a6b0
TheOddDyrus <Forever014>
2021-09-16 19:46:42 +0800

    add mybatis plus

---
+++ letsgo/src/main/java/com/thomax/letsgo/zoom/mybatis/MybatisPlusGenerator.java
@@ -0,0 +1,95 @@
+package com.thomax.letsgo.zoom.mybatis;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+import com.baomidou.mybatisplus.generator.config.GlobalConfig;
+import com.baomidou.mybatisplus.generator.config.PackageConfig;
+import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+import com.baomidou.mybatisplus.generator.config.rules.DateType;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+
+public class MybatisPlusGenerator {
+
+    public static final String author = "";
+
+    public static final String dbUrl = "";
+
+    public static final String userName = "";
+
+    public static final String password = "";
+
+    public static final String tableName = "";
+
+    public static final String tableNamePrefix = "";
+
+    public static final String packagePath = "";
+
+    public static final String controller = "";
+
+    public static final String entity = "";
+
+    public static final String service = "";
+
+    public static final String mapper = "";
+
+
+    public static void main(String[] args) {
+        // 1、创建代码生成器
+        AutoGenerator mpg = new AutoGenerator();
+
+        // 2、全局配置
+        GlobalConfig gc = new GlobalConfig();
+        String projectPath = System.getProperty("user.dir");
+        gc.setOutputDir(projectPath + "/src/main/java");
+        gc.setAuthor(author);
+        gc.setOpen(false); //生成后是否打开资源管理器
+        gc.setFileOverride(true); //重新生成时文件是否覆盖
+        gc.setServiceName("%sService"); //去掉Service接口的首字母I
+        gc.setIdType(IdType.ID_WORKER_STR); //主键策略
+        gc.setDateType(DateType.ONLY_DATE);//定义生成的实体类中日期类型
+        gc.setSwagger2(true);//开启Swagger2模式
+        gc.setBaseResultMap(true);
+        gc.setBaseColumnList(true);
+
+        mpg.setGlobalConfig(gc);
+
+        // 3、数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setUrl(dbUrl);
+        dsc.setDriverName("com.mysql.cj.jdbc.Driver");
+        dsc.setUsername(userName);
+        dsc.setPassword(password);
+        dsc.setDbType(DbType.MYSQL);
+        mpg.setDataSource(dsc);
+
+        // 4、包配置
+        PackageConfig pc = new PackageConfig();
+        pc.setModuleName(null); //模块名
+        pc.setParent(packagePath);
+        pc.setController(controller);
+        pc.setEntity(entity);
+        pc.setService(service);
+        pc.setMapper(mapper);
+        mpg.setPackageInfo(pc);
+
+        // 5、策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        strategy.setNaming(NamingStrategy.underline_to_camel);//数据库表映射到实体的命名策略
+        strategy.setInclude(tableName);//对哪一张表生成代码
+        strategy.setTablePrefix(tableNamePrefix); //生成实体时去掉表前缀
+
+        strategy.setColumnNaming(NamingStrategy.underline_to_camel);//数据库表字段映射到实体的命名策略
+        strategy.setEntityLombokModel(true); // lombok 模型 @Accessors(chain = true) setter链式操作
+
+        strategy.setRestControllerStyle(true); //restful api风格控制器
+        strategy.setControllerMappingHyphenStyle(true); //url中驼峰转连字符
+
+        mpg.setStrategy(strategy);
+
+        // 6、执行
+        mpg.execute();
+    }
+
+}
