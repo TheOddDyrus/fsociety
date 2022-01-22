56a2866add90a4837a953b65c1ee26e19e1ac439
TheOddDyrus <Forever014>
2022-01-22 15:43:55 +0800

    optimize

--- letsgo/src/main/java/com/thomax/letsgo/CodeX.java
+++ letsgo/src/main/java/com/thomax/letsgo/CodeX.java
@@ -1,5 +1,11 @@
 package com.thomax.letsgo;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.TreeMap;
+
 /**
  * Good luck have fun!!
  *
@@ -14,4 +20,55 @@ public class CodeX {
 }
 
 
+class MinStack {
 
+	private final Stack<Integer> stack;
+
+	private final TreeMap<Integer, Integer> minMap;
+
+	/** initialize your data structure here. */
+	public MinStack() {
+		stack = new Stack<>();
+		minMap = new TreeMap<>();
+	}
+
+	public void push(int x) {
+		stack.push(x);
+
+		Integer total = minMap.get(x);
+		if (total == null) {
+			total = 1;
+		} else {
+			total++;
+		}
+		minMap.put(x, total);
+	}
+
+	public void pop() {
+		Integer pop = stack.pop();
+
+		Integer total = minMap.get(pop);
+		if (total == 1) {
+			minMap.remove(pop);
+		} else {
+			minMap.put(pop, --total);
+		}
+	}
+
+	public int top() {
+		return stack.peek();
+	}
+
+	public int min() {
+		return minMap.firstKey();
+	}
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.min();
+ */
--- letsgo/src/main/java/com/thomax/letsgo/zoom/db/DBUtils.java
+++ letsgo/src/main/java/com/thomax/letsgo/zoom/db/DBUtils.java
@@ -1,153 +1,190 @@
 package com.thomax.letsgo.zoom.db;
 
-import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
-
-/**SUN公司制定了jdbc规范：
- * 		DriverManager类
- * 一套接口规范：
- * 		Connection 连接接口
- * 		Statement  命令接口
- * 		ResultSet  结果集接口
- * @author _thomas
+/**
+ * 原生数据库工具
+ *   Connection  连接接口
+ *   Statement  命令接口
+ *   ResultSet  结果集接口
  */
 public class DBUtils {
-	
-	private static String JDBC_DRIVER;
+
 	private static String DB_URL;
-	private static String USER;
-	private static String PWD;
+
+	private static String DB_USER;
+
+	private static String DB_PWD;
 	
-	public static ThreadLocal<Connection> localConn = new ThreadLocal<Connection>();
+	public static ThreadLocal<Connection> CONNECT_MANAGER = new ThreadLocal<>();
 	
 	static {
 		try {
-			/**使用ClassLoader的get Resource方法获得配置文件的io输入流*/
 			Properties prop = new Properties();
-			//InputStream is = ClassLoader.getSystemResourceAsStream("db.properties"); //读取src目录的文件
-			InputStream is = DBUtils.class.getClassLoader().getResourceAsStream("db.properties");
-			prop.load(is);
-			JDBC_DRIVER = prop.getProperty("db.driver");
-			DB_URL = prop.getProperty("db.url");
-			USER = prop.getProperty("db.user");
-			PWD = prop.getProperty("db.pwd");
+			prop.load(DBUtils.class.getClassLoader().getResourceAsStream("db.properties"));
 
-			/*
-			Class.forName("");的作用是要求JVM查找并加载指定的类，如果在类中有静态初始化器的话，JVM必然会执行该类的静态代码 段。
-			而在JDBC规范中明确要求这个Driver类必须向DriverManager注册自己，即任何一个JDBC Driver的 Driver类的代码都必须类似如下： 
-			DriverManager.registerDriver(new OracleDriver());  //等效于Class.forName("oracle.jdbc.driver.OracleDriver");
-			*/			
-			//将驱动类的class文件装载到内存中，并且形成一个描述此驱动类结构的Class类实例，并且初始化此驱动类，这样jvm就可以使用它了
-			Class.forName(JDBC_DRIVER);
+			DB_URL = prop.getProperty("db.url");
+			DB_USER = prop.getProperty("db.user");
+			DB_PWD = prop.getProperty("db.pwd");
+
+			Class.forName(prop.getProperty("db.driver"));
 		} catch (Exception e) {
-			System.out.println("DBUtils静态代码段异常：" + e.getMessage());
+			e.printStackTrace();
 		}
 	}
-	
+
+	public static void main(String[] args) {
+		//select
+		List<Result> select = select("SELECT xx FROM table1");
+
+		//modify
+		int row = modify("UPDATE table1 SET xx = xx WHERE xx");
+
+		//multi modify
+		try {
+			beginTransaction();
+			int row2 = multiModify("UPDATE table2 SET xx = xx WHERE xx");
+			//doing something
+			int row3 = multiModify("UPDATE table3 SET xx = xx WHERE xx");
+			commitTransaction();
+		} catch (Exception e) {
+			rollbackTransaction();
+		} finally {
+			closeTransaction();
+		}
+	}
+
 	/**
-	 * 适用于SELECT：
-	 * 后续用close(Statement stmt, Connection connection, ResultSet rs)来关闭此事务
-	 * @return Connection 非线程事务
+	 * SELECT
+	 */
+	public static List<Result> select(String sql) {
+		List<Result> resultList = new ArrayList<>();
+
+		try (Connection connection = getConnection();
+			 Statement statement = connection.createStatement();
+			 ResultSet resultSet = statement.executeQuery(sql)) {
+			while (resultSet.next()) {
+				Result result = new Result();
+				result.setId(resultSet.getObject("id", Long.class));
+				result.setName(resultSet.getObject("name", String.class));
+				resultList.add(result);
+			}
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+
+		return resultList;
+	}
+
+	/**
+	 * INSERT/UPDATE/DELETE（不需要事务）
+	 */
+	public static int modify(String sql) {
+		try (Connection connection = getConnection();
+			 Statement statement = connection.createStatement()) {
+			return statement.executeUpdate(sql);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	/**
+	 * INSERT/UPDATE/DELETE（需要事务）
+	 */
+	public static int multiModify(String sql) {
+		Connection connection = CONNECT_MANAGER.get();
+		try (Statement statement = connection.createStatement()) {
+			return statement.executeUpdate(sql);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	/**
+	 * 获得数据库连接
 	 */
 	public static Connection getConnection() {
 		try {
-			return DriverManager.getConnection(DB_URL, USER, PWD);
-		} catch (SQLException e) {
-			System.out.println("DriverManager获得Connection失败：" + e.getMessage());
+			return DriverManager.getConnection(DB_URL, DB_USER, DB_PWD);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
 		}
-		return null;
 	}
 
-	public static Connection getConnection2() {
-		try {
-			return DriverManager.getConnection(DB_URL + "另一个数据库url", USER, PWD);
-		} catch (SQLException e) {
-			System.out.println("DriverManager获得Connection失败：" + e.getMessage());
-		}
-		return null;
-	}
-	
 	/**
-	 * 关闭Statement，Connection，ResultSet
-	 * @param stmt
-	 * @param connection
-	 * @param rs
+	 * 开启事务
 	 */
-	public static void close(Statement stmt, Connection connection, ResultSet rs) {
+	public static void beginTransaction() {
 		try {
-			if (connection != null) {
-				connection.close();
-			}
-			if (stmt != null) {
-				stmt.close();
-			}
-			if (rs != null) {
-				rs.close();
-			}
+			Connection connection = getConnection();
+			connection.setAutoCommit(false);
+			CONNECT_MANAGER.set(connection);
 		} catch (SQLException e) {
-			System.out.println("JDBC规范接口关闭异常：" + e.getMessage());
-		}
-	}
-	
-	/**
-	 * 开启非自动Commit的事务保存至本地静态变量localConn
-	 */
-	public static void beginNoAutoTransaction() {
-		try {
-			Connection conn = DBUtils.getConnection();
-			conn.setAutoCommit(false);
-			localConn.set(conn);
-		} catch (SQLException se) {
-			System.out.println("开启非自动Commit事务发生异常：" + se.getMessage());
+			throw new RuntimeException(e);
 		}
 	}
 	
 	/**
 	 * 提交事务
 	 */
-	public static void commit() {
+	public static void commitTransaction() {
 		try {
-			Connection conn = localConn.get();
-			if (conn != null) {
-				conn.commit();
-				conn.close();
-				localConn.remove();
-			}
-		} catch (SQLException se) {
-			System.out.println("事务Commit时发生异常：" + se.getMessage());
+			Connection connection = CONNECT_MANAGER.get();
+			connection.commit();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
 		}
 	}
-	
+
 	/**
 	 * 回退事务
 	 */
-	public static void rollback() {
+	public static void rollbackTransaction() {
 		try {
-			Connection conn = localConn.get();
-			if (conn != null) {
-				conn.rollback();
-				conn.close();
-				localConn.remove();
-			}
-		} catch (SQLException se) {
-			System.out.println("事务rollback时发生异常：" + se.getMessage());
+			Connection connection = CONNECT_MANAGER.get();
+			connection.rollback();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
 		}
 	}
-	
+
 	/**
-	 * 适用于INSERT, DELETE, UPDATE：
-	 * 得到本地多线程事务；后续用必须要用commit()或rollback()来处理此事务，
-	 * 在使用commit()或rollback()时会关闭此事务
-	 * @return Connection 多线程事务
+	 * 关闭事务
 	 */
-	public static Connection getLocalConnection() {
-		return localConn.get();
+	public static void closeTransaction() {
+		try {
+			Connection connection = CONNECT_MANAGER.get();
+			connection.close();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		} finally {
+			CONNECT_MANAGER.remove();
+		}
+	}
+
+	public static class Result {
+		private Long Id;
+		private String Name;
+
+		public Long getId() {
+			return Id;
+		}
+		public void setId(Long id) {
+			Id = id;
+		}
+		public String getName() {
+			return Name;
+		}
+		public void setName(String name) {
+			Name = name;
+		}
 	}
 
 }
