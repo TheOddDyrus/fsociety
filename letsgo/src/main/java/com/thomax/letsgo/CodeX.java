7bdb211287acc174f7fa3ccca5930a35f01c032f
TheOddDyrus <Forever014>
2023-02-05 13:13:02 +0800

    remove example

--- letsgo/src/main/java/com/thomax/letsgo/CodeX.java
+++ letsgo/src/main/java/com/thomax/letsgo/CodeX.java
@@ -1,7 +1,5 @@
 package com.thomax.letsgo;
 
-import java.io.IOException;
-
 /**
  * Good luck have fun!!
  *
@@ -10,25 +8,7 @@ import java.io.IOException;
 public class CodeX {
 
 	public static void main(String[] args) {
-		sleep();
-	}
 
-	public static void sleep() {
-		System.out.print("正在启动...");
-		int seconds = 17 * 60;
-		for (int i = 0; i < seconds; i++) {
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				//
-			}
-		}
-
-		try {
-			Runtime.getRuntime().exec("shutdown -s -t 1");
-		} catch (IOException e) {
-			//
-		}
 	}
 
 }
